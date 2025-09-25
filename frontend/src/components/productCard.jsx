5e65698754824ae1ab494e8e9efb689b055c8cd9
yasasArt <yasasjayaweera945@gmail.com>
2025-09-25 13:46:48 +0530

    day10

---
+++ frontend/src/components/productCard.jsx
@@ -0,0 +1,11 @@
+export default function ProductCard(props){ //mul akura aniwryen capital wiya yuthui
+
+    console.log(props);
+
+    return(
+        <div>
+            <h1>Product Card Component</h1>
+            <p>This is a product card component.</p>
+        </div>
+    )
+}
