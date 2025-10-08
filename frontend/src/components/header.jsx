083aacd2d5c00f3d546ddcf2a219a6539de70f89
yasasArt <yasasjayaweera945@gmail.com>
2025-10-09 00:46:17 +0530

    start admin page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return(
-        <header className="w-full h-[100px] bg-primary">
-            <img src="/logo.jpg" className ="w-[100px]" alt="logo" />
-            <h1>My shop</h1>
+        <header className="w-full h-[100px] bg-secondary">
+            <img src="/logo.png" className ="w-[100px]" alt="logo" />
+            
             
 
         </header>
--- frontend/src/pages/adminPage.jsx
+++ frontend/src/pages/adminPage.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
+    <div className='w-full h-full flex bg-secondary'>
+      <div className="w-[300px] bg-black h-full">
+
+      </div>
+      <div className="w-[calc(100%-300px)] h-full border-[10px] bg-accent overflow-y-scroll border-secoundary rounded-2xl text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eaque sunt voluptatem esse ad blanditiis et sed, fugit excepturi sit quod ipsa quae, totam nobis expedita est consequatur harum distinctio? Dolor dolore itaque voluptatibus veniam nulla, quas atque expedita? Repellendus aspernatur neque amet vel, molestias, dolorum quis voluptatem dolores accusamus perspiciatis beatae nam atque at! Optio nisi ducimus labore molestias hic quam tenetur exercitationem quo sit sint, harum iusto. Laudantium, soluta! Id veniam aspernatur quia, ipsa modi maxime similique architecto hic earum officia aliquid culpa explicabo quae pariatur quos! Veritatis facilis alias vitae iusto, fuga ipsa quia consequuntur quam asperiores facere natus aut rem porro inventore, maxime unde quod quis praesentium amet numquam mollitia voluptatibus sint temporibus ut? Excepturi quaerat, harum repudiandae sapiente dolorum vel tenetur eius architecto soluta, nisi ducimus at. Exercitationem non aspernatur rem optio dolore tenetur placeat, labore corrupti nobis cum repudiandae velit hic, nihil commodi ducimus sit ipsa consectetur mollitia accusantium. Perferendis cum quasi amet eius rem et quibusdam ipsum! Saepe impedit ex deleniti! Rerum magnam nam ab quisquam molestiae soluta eius voluptatum commodi eligendi atque delectus, dicta a aperiam autem veritatis dolorum illum, doloremque ipsa suscipit eum molestias aspernatur facilis et. Neque saepe officiis quod!
+      </div>
+      
+
+    </div>
   )
 }
 
