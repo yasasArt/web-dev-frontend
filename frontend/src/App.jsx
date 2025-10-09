69233a8e8214f5b70ceafb0a929a3116f1659bb1
yasasArt <yasasjayaweera945@gmail.com>
2025-10-09 12:20:46 +0530

    admin link setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Route path='/' element={<HomePage/>} />
           <Route path='/login' element={<LoginPage/>} />
           <Route path='/register' element={<RegisterPage/>} />
-          <Route path='/admin' element={<AdminPage/>} />
+          <Route path='/admin/*' element={<AdminPage/>} />
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/pages/adminPage.jsx
+++ frontend/src/pages/adminPage.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 
 const AdminPage = () => {
   return (
     <div className='w-full h-full flex bg-secondary'>
       <div className="w-[300px] bg-black h-full">
 
+        <div className='w-full h-[100px] flex text-white items-center '>
+          <img src="/logo.png" alt="logo" className='h-full' />
+          <h1 className='text-2xl'> Admin </h1>
+        </div>
+
+        <div className='w-full h-[400px] text-white flex flex-col gap-5 text-2xl'>
+          <Link to="/admin">Orders</Link>
+          <Link to="/admin/products">Products</Link>
+          <Link to="/admin/users">Users</Link>
+          <Link to="/admin/reviews">Reviews</Link>
+        </div>
+
       </div>
+
       <div className="w-[calc(100%-300px)] h-full border-[10px] bg-accent overflow-y-scroll border-secoundary rounded-2xl text-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eaque sunt voluptatem esse ad blanditiis et sed, fugit excepturi sit quod ipsa quae, totam nobis expedita est consequatur harum distinctio? Dolor dolore itaque voluptatibus veniam nulla, quas atque expedita? Repellendus aspernatur neque amet vel, molestias, dolorum quis voluptatem dolores accusamus perspiciatis beatae nam atque at! Optio nisi ducimus labore molestias hic quam tenetur exercitationem quo sit sint, harum iusto. Laudantium, soluta! Id veniam aspernatur quia, ipsa modi maxime similique architecto hic earum officia aliquid culpa explicabo quae pariatur quos! Veritatis facilis alias vitae iusto, fuga ipsa quia consequuntur quam asperiores facere natus aut rem porro inventore, maxime unde quod quis praesentium amet numquam mollitia voluptatibus sint temporibus ut? Excepturi quaerat, harum repudiandae sapiente dolorum vel tenetur eius architecto soluta, nisi ducimus at. Exercitationem non aspernatur rem optio dolore tenetur placeat, labore corrupti nobis cum repudiandae velit hic, nihil commodi ducimus sit ipsa consectetur mollitia accusantium. Perferendis cum quasi amet eius rem et quibusdam ipsum! Saepe impedit ex deleniti! Rerum magnam nam ab quisquam molestiae soluta eius voluptatum commodi eligendi atque delectus, dicta a aperiam autem veritatis dolorum illum, doloremque ipsa suscipit eum molestias aspernatur facilis et. Neque saepe officiis quod!
+        {/* path eka dnnma one neh */}
+         <Routes path="/"> 
+              <Route path='/' element={<h1>Orders</h1>} />
+              <Route path='/products' element={<h1>product</h1>} />
+              <Route path='/users' element={<h1>users</h1>} />
+              <Route path='/reviews' element={<h1>Reviews</h1>} />
+         </Routes>
       </div>
       
 
