ab4d078d0c623a22e4f203923cd0fc1135b5c132
yasasArt <yasasjayaweera945@gmail.com>
2025-10-21 16:30:15 +0530

    cors, env, notification

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login(){
+      console.log("Login button clicked");
+      console.log("Email:", email);
+      console.log("password:", password);
+
+      try {
+        
+        const res = await axios.post(import.meta.env.VITE_BACKEND_URL + "/users/login", {
+          email: email,
+          password: password,
+        })
+        console.log(res);
+
+        if(res.data.role == "Admin"){
+          navigate("/admin");
+        }else{
+          navigate("/");
+        }
+
+        toast.success("Login successful!");
+
+      } catch (error) {
+
+        toast.error("Login failed. Please check your credentials.");
+        
+        console.log("Error during login:");
+        console.log(error);
+      }
+  }
+
   return (
     <div className="w-full h-screen bg-[url('/Bg.jpg')] bg-center bg-cover bg-no-repeat flex">
       <div className="w-[50%] h-full flex justify-center items-center flex-col p-[50px]">
@@ -14,11 +51,13 @@ const LoginPage = () => {
         <h1 className="text-[50px] text-MainText text-shadow-secondary text-shadow-2xs text-center font-bold">
           Pug In. Power Up. Play Hard.
         </h1>
+
         <p className="text-[20px] text-secondary mt-[20px] mb-[40px] italic text-center">
           Join the ultimate gaming community. Log in to access exclusive
           features.
         </p>
       </div>
+
       <div className="w-[50%] h-full flex justify-center items-center">
         <div className="w-[450px] h-[600px] backdrop-blur-2xl shadow-2xl rounded-2xl flex flex-col justify-center items-center ">
           <h1 className="text-[40px] font-bold mb-[20px] text-white text-shadow-white">
@@ -26,11 +65,21 @@ const LoginPage = () => {
             Login
           </h1>
           <input
+            onChange={
+              (e) =>{
+                setEmail(e.target.value)
+              }}
+
             type="email"
             placeholder="your email"
             className="w-[400px] h-[50px] mb-[20px] rounded-lg border border-secondary p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-MainText"
           />
           <input
+            onChange={
+              (e) =>{
+                setPassword(e.target.value)
+              }}
+
             type="password"
             placeholder="Password"
             className="w-[400px] h-[50px] mb-[20px] rounded-lg border border-secondary p-[10px] text-[20px] focus:outline-none focus:ring-2 focus:ring-MainText"
@@ -40,9 +89,8 @@ const LoginPage = () => {
             <Link to="forgot-password" className="text-MainText italic">
                 Reset it here
             </Link>
-
           </p>
-          <button className="w-[400px] h-[50px] bg-MainText text-white font-bold text-[20px] rounded-lg border-[2px] border-secondary hover:bg-transparent hover:text-secondary ">
+          <button onClick={login} className="w-[400px] h-[50px] bg-MainText text-white font-bold text-[20px] rounded-lg border-[2px] border-secondary hover:bg-transparent hover:text-secondary ">
             Login
           </button>
           <p className="text-white not-italic text-center">
