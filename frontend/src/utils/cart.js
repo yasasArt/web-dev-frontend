3b3e433388a74705d1c9337ac7552f5c750cb9ce
yasasArt <yasasjayaweera945@gmail.com>
2025-12-09 17:26:04 +0530

    cart page done

--- frontend/src/utils/cart.js
+++ frontend/src/utils/cart.js
@@ -1,12 +1,12 @@
 import { toast } from "react-hot-toast";
 
 export function getCart(){
-    let cartString = localStorage.getItem('cart');//get cart string from local storage
+    let cartString = localStorage.getItem('cart'); //get cart string from local storage
 
     if(cartString == null){
         localStorage.setItem('cart', "[]");
         return []; 
-    }else{
+    } else {
         const cart = JSON.parse(cartString);
         return cart;
     }
@@ -18,10 +18,11 @@ export function addToCart(product, quantity){
     //check if product already in cart
     const index = cart.findIndex(
         (item)=>{
-            return item.productID == product.productID
+            return item.productID == product.productID;
         }
-    ) 
-    if(index != -1){
+    );
+    
+    if(index == -1){ // Product NOT found, add new item
         cart.push({
             productID: product.productID,
             name: product.name,
@@ -29,20 +30,21 @@ export function addToCart(product, quantity){
             labelledPrice: product.labelledPrice,
             quantity: quantity,
             image: product.images[0]
-        })
+        });
         toast.success(`${product.name} added to cart`);
-    }else{
+    } else { // Product found, update quantity
         const newQty = cart[index].quantity + quantity;
 
         if(newQty <= 0){
             cart.splice(index, 1); //remove item from cart
             toast.success(`${product.name} removed from cart`);
-        }else{
+        } else {
             cart[index].quantity = newQty;
             toast.success(`Updated ${product.name} quantity to ${newQty}`);
         }
-   }
-   const cartString = JSON.stringify(cart);
+    }
+    
+    const cartString = JSON.stringify(cart);
     localStorage.setItem('cart', cartString);
 }
 
