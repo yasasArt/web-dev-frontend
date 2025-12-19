7e93a8601680fb577babf11d95955189f70610e0
yasasArt <yasasjayaweera945@gmail.com>
2025-12-19 10:50:39 +0530

    reset Password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { Toaster } from 'react-hot-toast'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import ForgetPasswordPage from './pages/forgetPasswordPage.jsx'
 
+
 // 491094543732-m8sb4v5r6bq6sgcg6qc2fuijsbtqf2l7.apps.googleusercontent.com
 
 function App() {
@@ -21,11 +22,11 @@ function App() {
       <div className='w-full h-screen bg-accent text-secondary'>
         <Routes>
           <Route path='/*' element={<HomePage/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/register' element={<RegisterPage/>} />
-          <Route path='/admin/*' element={<AdminPage/>} />
-          <Route path='/test' element={<TestPage/>} />
-          <Route path="/forgot-password" element={<ForgetPasswordPage/>}/>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login/forgot-password" element={<ForgetPasswordPage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+          <Route path="/test" element={<TestPage />} />
         </Routes>
       </div>
     </BrowserRouter>
